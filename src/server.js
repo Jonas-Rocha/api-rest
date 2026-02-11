ec259d9161f396f343c1f93fd8fda8190c1afd41
Jonas Rocha <jonasrpnstudy@gmail.com>
2026-02-11 14:29:20 -0300

    Adicionando com Typescript

--- src/server.js
+++ src/server.js
@@ -1,1 +1,13 @@
-console.log("Hello World!");
+/*
+INFORMAÇÕES ANTERIORES AO INÍCIO DO PROJETO:
+
+rodei npm i typescript@5.5.4 @types/node@20.14.12 -D
+
+npm i   <<<< para instalar
+typescript@5.5.4   <<<< instala a biblioteca versão 5.5.4 do typescript no projeto
+@types/node@20.14.12   <<<< para adicionar também a typagem do node para utilizar o typescript para o node, e o 20.14.12 é a versão dessa typagem
+-D   <<<< é para instalar as duas dependencias(bibliotecas) como bibliotecas de DESENVOLVIMENTO, eu seja, ele vai apenas usar o node enquanto estamos em ambiente de DESENVOLVIMENTO. e quando for em produção não precisar mais. 
+pois no fim das contas tudo  é convertido em javascript.  o typescript no fim das contas converte todo o código para javascript então não precisaremos dessas duas dependencias se tudo no fim é convertido para javascript.
+
+no fim, o typescript aqui no ambiente de desenvolvimento é apenas para auxiliar na tipagem e reduzir os erros como passar um número no lugar de uma string, por exemplo.
+*/
