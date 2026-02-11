c7c232153914a15b31dfd0076e06f50b0b3560e5
Jonas Rocha <jonasrpnstudy@gmail.com>
2026-02-11 15:23:45 -0300

    Compilando TypesScript

--- src/server.ts
+++ src/server.ts
@@ -6,11 +6,10 @@ rodei npm i typescript@5.5.4 @types/node@20.14.12 -D
 npm i   <<<< para instalar
 typescript@5.5.4   <<<< instala a biblioteca versão 5.5.4 do typescript no projeto
 @types/node@20.14.12   <<<< para adicionar também a typagem do node para utilizar o typescript para o node, e o 20.14.12 é a versão dessa typagem
--D   <<<< é para instalar as duas dependencias(bibliotecas) como bibliotecas de DESENVOLVIMENTO, eu seja, ele vai apenas usar o node enquanto estamos em ambiente de DESENVOLVIMENTO. e quando for em produção não precisar mais.
+-D   <<<< é para instalar as duas dependencias(bibliotecas) como bibliotecas de DESENVOLVIMENTO, eu seja, ele vai apenas usar o node enquanto estamos em ambiente de DESENVOLVIMENTO. e quando for em produção não precisar mais. 
 pois no fim das contas tudo  é convertido em javascript.  o typescript no fim das contas converte todo o código para javascript então não precisaremos dessas duas dependencias se tudo no fim é convertido para javascript.
 
 no fim, o typescript aqui no ambiente de desenvolvimento é apenas para auxiliar na tipagem e reduzir os erros como passar um número no lugar de uma string, por exemplo.
-
 
 
 
@@ -22,9 +21,13 @@ isso explica primeira anotação la em cima que diz que o typescript é apenas p
 afinal, ele não é muito compativel com o node, então precisamos converter para javascript antes de colocar em produção.
 o typescript é apenas para conseguirmos fazer tudo muito bem estruturado e não deixar erros.
 
+
 */
-function sum(a, b) {
+
+function sum(a: number, b: number): number {
   return a + b;
 }
-var result = sum(5, 3);
+
+const result: number = sum(5, 3);
+
 console.log(result);
