d000cf0e9295a0c1f4aa838ec647d2b89fe2b3d8
Jonas Rocha <jonasrpnstudy@gmail.com>
2026-02-17 15:04:36 -0300

    Tratando a requisição

--- src/utils/AppError.ts
+++ src/utils/AppError.ts
@@ -4,6 +4,7 @@ class AppError {
   message: string;
   statusCode: number;
 
+  // statusCode: number = 400, como eu estou definindo um valor padrão pro statusCode ele vira opcional na hora da chamada, pois ele sempre vai ser 400 quando nao informado.
   constructor(message: string, statusCode: number = 400) {
     //quando uso o this.variavel, eu estou me referindo a variavel global da classe.
     //já no construtor, se refere ao que vai ser instanciado(uma nova cópia)
